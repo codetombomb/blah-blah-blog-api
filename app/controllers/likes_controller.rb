7190b1cd6884ee4a3d3d2c06aea702ef600efcd3
Tom Tobar <cocdetombomb@gmail.com>
2023-06-28 16:53:59 -0500

    add toggle-like on likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+    def toggle_like
+        like = Like.find_by(like_params)
+        if like
+            like.destroy
+            render json: {message: "Un-liking"}
+        else 
+            new_like = Like.create!(like_params)            
+            render json: {message: "Liking", like_id: new_like.id}
+        end
+    end
+
+    private 
+
+    def like_params
+      params.permit(:user_id, :blog_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   resources :tags
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :likes
   resources :blogs
-
+  
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/me", to: "users#show"
+
+  post "/toggle-like", to: "likes#toggle_like"
 end
