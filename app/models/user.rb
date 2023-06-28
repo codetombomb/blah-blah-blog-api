e49dbf8555ae0cccd54e51e450e2182a9d82dff8
Tom Tobar <cocdetombomb@gmail.com>
2023-06-27 22:52:08 -0500

    add blogs to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :blogs
   
   has_many :likes, 
     dependent: :destroy
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :first_name, :last_name, :num_followers
   has_many :followers
+  has_many :blogs
 
   def num_followers
     self.object.followers.length
