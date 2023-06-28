ba0c2cf6f3f2293b454aec7b13bc0a3fe4b7457c
Tom Tobar <cocdetombomb@gmail.com>
2023-06-27 22:56:35 -0500

    add following to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :num_followers
+  attributes :id, :email, :first_name, :last_name, :num_followers, :following
   has_many :followers
   has_many :blogs
+  has_many :liked_blogs
 
   def num_followers
     self.object.followers.length
+  end
+
+  def following
+    self.object.followees
   end
 end
